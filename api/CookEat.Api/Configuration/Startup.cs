bd9a63ec144b6284e66088580c53f7fac2749893
Sullivan Lacheteau <lacheteau.s@gmail.com>
2022-09-24 00:39:30 -0400

    Startup configuration

---
+++ api/CookEat.Api/Configuration/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace CookEat.Api.Configuration
+{
+    public static class Startup
+    {
+        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder;
+        }
+
+        public static WebApplication ConfigureMiddlewares(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
