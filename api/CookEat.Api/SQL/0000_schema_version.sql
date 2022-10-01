0285c08e4b9e29c7de43ea868d2f756ca5f2db73
Sullivan Lacheteau <lacheteau.s@gmail.com>
2022-10-01 00:31:39 -0400

    1st script: schema version table creation

--- api/CookEat.Api/SQL/0000_schema_version.sql
+++ api/CookEat.Api/SQL/0000_schema_version.sql
@@ -1,1 +1,10 @@
-﻿
+﻿DROP TABLE IF EXISTS schema_version;
+CREATE TABLE schema_version
+(
+	version int NOT NULL,
+	update_date DATETIME DEFAULT GETUTCDATE() NOT NULL,
+	file_name VARCHAR(64) NOT NULL
+);
+
+DROP INDEX IF EXISTS schema_version_index ON schema_version;
+CREATE UNIQUE INDEX schema_version_index ON schema_version(version);
